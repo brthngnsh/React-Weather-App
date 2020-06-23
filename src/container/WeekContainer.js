b97dd2aa92efa66d605aee51e6992c8e68454e65
barathan ganesh <demongod250299@gmail.com>
2020-06-23 17:42:21 +0530

    added openweather api

--- src/container/WeekContainer.js
+++ src/container/WeekContainer.js
@@ -1,7 +1,20 @@
 import React, { Component } from 'react';
+import { apiConfig } from "../apiKey/apikey";
 
 class WeekContainer extends Component {
+    state = {
+        fullData: [],
+        dailyData: []
+    }
+
+    componentDidMount = () => {
+        const weatherURL = 'http://api.openweathermap.org/data/2.5/forecast?id=1273865&units=imperial&APPID=${apiConfig.owmKey}'
+        
+    }
+
     render() {
+
+        
         return(
             <div>
                 <h1>Hello World!</h1>
